bec77f8b91a92a84ff02fd0c765bfdd0e08efb75
zanzer-tem <dan.needham@gmail.com>
2020-07-13 12:12:23 +0100

    Updates

---
+++ precache-manifest.35bf90e436f0f6c4348c23885e1e608d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79928976bc1dc381dd9c934a91202f89",
+    "url": "/5e-deck-of-many-things/index.html"
+  },
+  {
+    "revision": "f26ceff184f2c6a17bf6",
+    "url": "/5e-deck-of-many-things/static/css/main.8e05b2b8.chunk.css"
+  },
+  {
+    "revision": "62548cd1431102694ed7",
+    "url": "/5e-deck-of-many-things/static/js/2.60052634.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/5e-deck-of-many-things/static/js/2.60052634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f26ceff184f2c6a17bf6",
+    "url": "/5e-deck-of-many-things/static/js/main.27c3d721.chunk.js"
+  },
+  {
+    "revision": "c9ef19909057459ba895",
+    "url": "/5e-deck-of-many-things/static/js/runtime-main.5c74d0a5.js"
+  }
+]);
